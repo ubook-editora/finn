011be9bebff230b0c4e6546bf28137db8d0e2a75
Hien Nguyen <hien.nguyenle.it@gmail.com>
2020-03-07 14:33:49 +0700

    refactor structure.

---
+++ src/source/test/djinni_ios_example/DjinniExample/generated-src/cpp/hello_impl.cpp
@@ -0,0 +1,39 @@
+//
+//  hello_impl.cpp
+//  djinni_ios_example
+//
+//  Created by Mr Hien Nguyen Le Hien on 3/4/20.
+//  Copyright © 2020 Fossil. All rights reserved.
+//
+
+#include "hello_impl.hpp"
+#include "my_record.hpp"
+
+//--cpp-namespace "demo"
+
+namespace demo {
+
+HelloImpl::HelloImpl() {
+}
+
+std::shared_ptr<Hello> Hello::create() {
+    return std::make_shared<HelloImpl>();
+}
+
+//MyRecord HelloImpl::say_hello() {
+//    return MyRecord(1000);
+//}
+
+MyRecord HelloImpl::say_hello(my_enum en) {
+    return MyRecord(1000);
+}
+
+my_enum HelloImpl::say_hi() {
+    return (my_enum)0;
+}
+
+void HelloImpl::hlll() {
+    printf("Ahihi");
+}
+
+}
---
+++ src/source/test/djinni_ios_example/DjinniExample/generated-src/cpp/hello_impl.hpp
@@ -0,0 +1,33 @@
+//
+//  hello_impl.hpp
+//  djinni_ios_example
+//
+//  Created by Mr Hien Nguyen Le Hien on 3/4/20.
+//  Copyright © 2020 Fossil. All rights reserved.
+//
+
+#ifndef hello_impl_hpp
+#define hello_impl_hpp
+
+#include <stdio.h>
+#include "hello.hpp"
+
+namespace demo {
+
+class HelloImpl: public Hello {
+    
+public:
+    HelloImpl();
+    
+//    virtual std::shared_ptr<Hello> create() override;
+    
+//    virtual MyRecord say_hello() override;
+    
+    virtual MyRecord say_hello(my_enum en) override;
+    
+    virtual my_enum say_hi() override;
+    
+    virtual void hlll() override;
+};
+}
+#endif /* hello_impl_hpp */
